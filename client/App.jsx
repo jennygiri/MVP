9d08a4b4e16265cbfae385fdd5a80eaad1b6f771
Jennifer Giri <jenniferanngiri@gmail.com>
2022-03-14 20:31:45 -0400

    environment setup

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = (props) => {
+  return (
+    <div>hey there</div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  mode: "development"
+};
+
+module.exports = config;
